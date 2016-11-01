18bbb27d00e5d751cea4bea97f8a54a53f485834
Morteza Araby <morteza.araby@ericsson.com>
2016-11-01 12:38:21 +0100

    Style the Main and Example page.

--- src/components/Examples.js
+++ src/components/Examples.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import {Link} from 'react-router'
 
 const Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example location to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Stockholm'>Stockholm, Sweden</Link>
+        </li>
+        <li>
+          <Link to='/?location=Rio'>Rio, Brazil</Link>
+        </li>
+      </ol>
     </div>
   )
 }
